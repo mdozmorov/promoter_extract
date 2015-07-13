abfa3a8210708aa2d89ce55d69b75f1bfeedfe09
Mikhail Dozmorov <mikhail.dozmorov@gmail.com>
2015-07-13 19:59:09 -0400

    Adding recommended workflow

--- make.sh
+++ make.sh
@@ -1,7 +1,12 @@
 # Get full list of gene names from HGNC
 wget ftp://ftp.ebi.ac.uk/pub/databases/genenames/new/tsv/hgnc_complete_set.txt
 gzip hgnc_complete_set.txt
-mv gnc_complete_set.txt.gz txt/
+mv hgnc_complete_set.txt.gz txt/
 # List of gene names
 zcat < txt/hgnc_complete_set.txt.gz | sed '1d' | cut -f2 > txt/all.hgnc_symbol.txt
 python refgene2.py txt/all.hgnc_symbol.txt | bedtools sort -i - | mergeBed -s -c 4 -o distinct -i - > all.hgnc_symbol.bed
+
+# Recommended:
+# Split the list of all genes into chunks
+# run promoter extraction on each chunks
+# merge chunks
